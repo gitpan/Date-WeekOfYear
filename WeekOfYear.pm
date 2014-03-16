d2e94a7acc48a625f5b0c4193cebe8e2f75c6def
Greg George <gng@cpan.org>
2014-03-16 10:51:33 +0000

    Import of GNG/Date-WeekOfYear-1.05 from CPAN.
    gitpan-cpan-distribution: Date-WeekOfYear
    gitpan-cpan-version:      1.05
    gitpan-cpan-path:         GNG/Date-WeekOfYear-1.05.tar.gz
    gitpan-cpan-author:       GNG
    gitpan-cpan-maturity:     released

--- WeekOfYear.pm
+++ WeekOfYear.pm
@@ -5,36 +5,85 @@
 #
 package Date::WeekOfYear;
 
-use 5.006;
 use strict;
 use warnings;
 use Time::Local;
 
+our $VERSION = '1.05';
+
 require Exporter;
 
 our @ISA = qw(Exporter);
-
 our %EXPORT_TAGS = ( 'all' => [ qw( WeekOfYear ) ] );
+our @EXPORT_OK   = ( @{ $EXPORT_TAGS{'all'} } );
+our @EXPORT      = qw( WeekOfYear );
 
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-    WeekOfYear
-);
-our $VERSION = sprintf("%d.%02d", q'$Revision: 1.4 $' =~ /(\d+)\.(\d+)/);
-
+# Weekday constants
+use constant {
+    SUNDAY    => 0,
+    MONDAY    => 1,
+    TUESDAy   => 2,
+    WEDNESDAY => 3,
+    THURSDAY  => 4,
+    FRIDAY    => 5,
+    SATURDAY  => 6,
+    };
 
 sub WeekOfYear
 {
-    my $time = $_[0] || time;
+    my ($time, $compat) = @_;
 
-    #-- Current day & time
-    my ($tm_year, $wkday, $yrday) = (localtime($time))[5..7];
+    # Set to the current time if nothing provided
+    $time = time unless (defined($time) && $time =~ /^\s*\d+\s*$/);
 
+    # wkday is the day of the week, 0=Sunday, 1=Monday.. 4=Thursday
+    my ($tm_day, $tm_mth, $tm_year, $wkday, $yrday) = (localtime($time))[3..7];
+
+    # 1 Jan of the year in question
     my $startOfYear = timelocal(0, 0, 0, 1, 0, $tm_year);
-    my ($soywkday) = (localtime($startOfYear))[6];
 
-    my $wkNo = int($yrday / 7) + 1 + (($wkday < $soywkday)? 1:0);
+    # What is the week day for 1 Jan of the week in question
+    my ($soywkday)  = (localtime($startOfYear))[6];
+    my $wkNo;
+
+    if ($compat)
+    {
+        # Pre version 1.4 functionality
+        $wkNo = int($yrday / 7) + 1 + (($wkday < $soywkday)? 1:0);
+    }
+    else
+    {
+        # Version 1.5 and beyound ISO 8601 based week number
+        # Week 1 is the first week of the year containing a Thursday
+        # Is the first day of the week Friday or greater, if so this must be week 2
+        my $offset = ($soywkday > THURSDAY) ? 1 : 0;
+
+        # Note, yrday starts at 0, ie 1 Jan has yrday = 0
+        if ($offset)
+        {
+            # 1 Jan is in week 53 of the previous year
+            $wkNo = 53 + int(($yrday + $soywkday - 1)/ 7);
+        }
+        else
+        {
+            $wkNo = 1 + int(($yrday + $soywkday - 1)/ 7);
+        }
+
+        if ($wkNo == 53)
+        {
+            if ($tm_mth == 11 && $wkday < THURSDAY && $wkday > 0 && $tm_day >= 29)
+            {
+                $wkNo = 1;
+                $tm_year++;
+            }
+            elsif ($offset && $tm_mth == 0 && (($soywkday == SUNDAY && $yrday < 1) || ($soywkday == SATURDAY && $yrday < 2) || ($soywkday == FRIDAY && $yrday < 3)))
+            {
+                $tm_year--;
+            }
+        }
+
+        $wkNo -= 53 if ($wkNo >  53);
+    }
 
     return wantarray ? ($wkNo, $tm_year + 1900) : $wkNo;
 }
@@ -45,7 +94,7 @@ __END__
 
 =head1 NAME
 
-Date::WeekOfYear - Simple routine to return the week of the year (as well as the year)
+Date::WeekOfYear - Simple routine to return the ISO 8601 week of the year (as well as the ISO week year)
 
 =head1 SYNOPSIS
 
@@ -75,11 +124,44 @@ In a scalar context, just the week number is returned (the year starts at week 1
 In a list context, both the week number and the year (YYYY) are returned.  This
 ensures that you know which year the week number relates too.  This is only an
 issue in the week where the year changes (ie depending on the day you can be in
-either week 52  or week 1.
+either week 52, week 53 or week 1.
 
-Note there if you are after other date related functions then there are plenty
-of other Date::* modules on CPAN provide the functionality of this module in
-addition other ...
+B<NOTE> The year returned is not always the same as the Gregorian year for that day
+for further details see ISO 8601.
+
+=head1 CHANGES
+
+As of version 1.5 the ISO 8601 week number is calculated.  For backwards compatibility
+a flag can be passed after the time to give the previous functionality.
+
+For example:
+
+  my $weekNo = WeekOfYear(undef, 1);  # Week number now in pre ISO 8601 mode
+  or
+  my $weekNo = WeekOfYear($the_time, 1);  # Week number for $the_time in pre ISO 8601 mode
+
+
+=head1 ISO 8601
+
+Weeks in a Gregorian calendar year can be numbered for each year. This style of
+numbering is commonly used (for example, by schools and businesses) in some European
+and Asian countries, but rare elsewhere.
+
+ISO 8601 includes the ISO week date system, a numbering system for weeks - each week
+begins on a Monday and is associated with the year that contains that week's Thursday
+(so that if a year starts in a long weekend Friday-Sunday, week number one of the year
+will start after that). For example, week 1 of 2004 (2004W01) ran from Monday 29
+December 2003 to Sunday, 4 January 2004, because its Thursday was 1 January 2004,
+whereas week 1 of 2005 (2005W01) ran from Monday 3 January 2005 to Sunday 9 January
+2005, because its Thursday was 6 January 2005 and so the first Thursday of 2005. The
+highest week number in a year is either 52 or 53 (it was 53 in the year 2004).
+
+An ISO week-numbering year (also called ISO year informally) has 52 or 53 full weeks.
+That is 364 or 371 days instead of the usual 365 or 366 days.  The extra week is
+referred to here as a leap week, although ISO 8601 does not use this term.  Weeks start
+with Monday. The first week of a year is the week that contains the first Thursday
+(and, hence, 4 January) of the year. ISO week year numbering therefore slightly
+deviates from the Gregorian for some days close to 1 January.
 
 =head1 EXPORT
 
@@ -89,9 +171,11 @@ WeekOfYear.  That's it, nice and simple
 
 =head1 KNOWN ISSUES
 
+B<Versions prior to 1.5 did not follow ISO 8601.>
+
 None, however please contact the author at gng@cpan.org should you
 find any problems and I will endevour to resolve then as soon as
-possible
+possible.
 
 =head1 AUTHOR
 
@@ -105,10 +189,14 @@ Date::Parse or check CPAN http://search.cpan.org/search?query=Date&mode=all
 =head1 ACKNOWLEDGEMENTS
 
 Thanks to Alexandr Ciornii for the V1.3 updates
+Thanks to Niel Bowers for [rt.cpan.org #93599] Not clear what type of week number is returned
 
 =head1 Log
 
-$Log: WeekOfYear.pm,v $
+Revision 1.5  2014/03/16 Greg
+ - Updated to conform to ISO 8601
+ - Added compatability flag to allow backwards usage
+
 Revision 1.4  2009/06/21 07:29:05  Greg
 - Added ACKNOWLEDGEMENTS
 
--- t/test.t
+++ t/test.t
@@ -3,29 +3,88 @@ use strict;
 use warnings;
 
 use Test::More;
-plan tests => 8;
+plan tests => 33;
 
 use Date::WeekOfYear;
 use Time::Local;
 
-ok(1);
-my $time = timelocal(0,0,12,1,0,108);
-is(WeekOfYear($time),1);
+ok(1, 'Date::WeekOfYear compiled, a good start');
+##2
+#my $time = timelocal(0,0,12,1,0,108);
+#is(WeekOfYear($time),1);
+#
+##3
+#$time = timelocal(0,0,12,5,0,108); #Saturday
+#is(WeekOfYear($time),1);
+#
+##4
+#$time = timelocal(0,0,12,29,0,108);
+#is(WeekOfYear($time),5);
+#
+##5
+#$time = timelocal(0,0,12,1,1,108);
+#is(WeekOfYear($time),5);
+#
+##6
+#$time = timelocal(0,0,12,1,0,116);
+#is(WeekOfYear($time),1);
+#
+##7
+#$time = timelocal(0,0,12,2,0,116);
+#is(WeekOfYear($time),1);
+#
+##8
+#$time = timelocal(0,0,12,6,0,116);
+#is(WeekOfYear($time),2);
+#
+##9,10
+## 2005-01-01    2004-W53-6
+#$time = timelocal(0,0,12,6,0,2005-1900);
+#my ($wk_num, $year) = WeekOfYear($time);
+#is($wk_num, 53);
+#is($year, 2004);
 
-$time = timelocal(0,0,12,5,0,108); #Saturday
-is(WeekOfYear($time),1);
+my @tests = (
+    # Date,            YYYY-MM-DD,   ISO Wk YYYY-WkNo-Day, Comment
+    ['Sat 1 Jan 2005',  '2005-01-01', '2004-W53-6', '',],
+    ['Sun 2 Jan 2005',  '2005-01-02', '2004-W53-7', '',],
+    ['Sat 31 Dec 2005', '2005-12-31', '2005-W52-6', '',],
+    ['Mon 1 Jan 2007',  '2007-01-01', '2007-W01-1', 'Both years 2007 start with the same day.',],
+    ['Sun 30 Dec 2007', '2007-12-30', '2007-W52-7', '',],
+    ['Mon 31 Dec 2007', '2007-12-31', '2008-W01-1', '',],
+    ['Tue 1 Jan 2008',  '2008-01-01', '2008-W01-2', 'Gregorian year 2008 is a leap year, ISO year 2008 is 2 days shorter: 1 day longer at the start, 3 days shorter at the end.',],
+    ['Sun 28 Dec 2008', '2008-12-28', '2008-W52-7', 'The ISO year 2009 is 3 days into the previous Gregorian year.',],
+    ['Mon 29 Dec 2008', '2008-12-29', '2009-W01-1', '',],
+    ['Tue 30 Dec 2008', '2008-12-30', '2009-W01-2', '',],
+    ['Wed 31 Dec 2008', '2008-12-31', '2009-W01-3', '',],
+    ['Thu 1 Jan 2009',  '2009-01-01', '2009-W01-4', '',],
+    ['Thu 31 Dec 2009', '2009-12-31', '2009-W53-4', 'ISO year 2009 has 53 weeks, thus it is 3 days into the Gregorian year 2010.',],
+    ['Fri 1 Jan 2010',  '2010-01-01', '2009-W53-5', '',],
+    ['Sat 2 Jan 2010',  '2010-01-02', '2009-W53-6', '',],
+    ['Sun 3 Jan 2010',  '2010-01-03', '2009-W53-7', '',],
+    );
 
-$time = timelocal(0,0,12,29,0,108);
-is(WeekOfYear($time),5);
+# Run through the test cases
+foreach my $t (@tests)
+{
+    my $comment = $t->[0];
+    $comment .= ' - ' . $t->[2] if $t->[2];
+    $comment .= ' - ' . $t->[3] if $t->[3];
 
-$time = timelocal(0,0,12,1,1,108);
-is(WeekOfYear($time),5);
+    # Generate the time value. Use midday so we don't run into any daylight saving time issues
+    #$time = timelocal($sec,$min,$hour,$mday,$mon,$year);
+    my ($year, $mon, $mday) = split /-/, $t->[1];
+    my $time = timelocal(0, 0, 12, $mday, $mon - 1, $year - 1900);
 
-$time = timelocal(0,0,12,1,0,116);
-is(WeekOfYear($time),1);
+    # Get the required answer
+    my ($wk_num_year_answer, $wk_num_answer, $day_answer) = split /-/, $t->[2];
+    $wk_num_answer =~ s/W0*//;
 
-$time = timelocal(0,0,12,2,0,116);
-is(WeekOfYear($time),1);
+    # Run the test
+    my ($wk_num, $wk_num_year) = WeekOfYear($time);
 
-$time = timelocal(0,0,12,6,0,116);
-is(WeekOfYear($time),2);
+    # Check the results for week number and year
+    #diag($t->[0] . ' --> ' . scalar(localtime($time)));
+    is ($wk_num, $wk_num_answer, 'Wk Num test - ' . $comment);
+    is ($wk_num_year, $wk_num_year_answer, 'Year test - ' . $comment);
+}
